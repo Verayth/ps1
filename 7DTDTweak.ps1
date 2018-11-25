a57a7e5abb30ea636343ad683f0567265c7e7e9c
Verayth <Verayth@users.noreply.github.com>
2018-11-24 22:03:09 -0500

    7DTDT: modified OP to have cheat/NonCheat.  Added Corpses Last Longer

--- 7DTDTweak.ps1
+++ 7DTDTweak.ps1
@@ -5,8 +5,9 @@
 This is a program for applying Tweaks to the XML files for the game 7 Days to Die.  This is as close to "modding" as you can get with games that only allow XML edits.
 .NOTES
 20181118: started project
-20181123: first working version with successful in-game testing
+20181123: first working version with successful in-game testing (OPTools,OPArmor,Buffs)
 20181124: 1.0
+20181124a: added Corpses Last Longer
 #>
 [CmdletBinding()]param(
     #The 7DTD Program Directory.  use this parm if the registry query fails.
@@ -16,7 +17,7 @@ This is a program for applying Tweaks to the XML files for the game 7 Days to Di
 $header=@"
 ##################################################################
 # 7 Days to Die - XML Modding Tool                               #
-# by Verayth - Version 1.0.181124                                #
+# by Verayth - Version 1.1.181124a                               #
 ##################################################################
 "@
 $ErrorActionPreference='Stop'
@@ -47,19 +48,24 @@ if ($progdir) {
 
 #$FileVersion = "$progdir\7DaysToDie.exe" | % VersionInfo | % FileVersion
 
-$LootXml = New-Object System.Xml.XmlDocument
-$LootXml.PreserveWhitespace = $true
-$LootXml.Load("$progdir\Data\Config\loot.xml")
+$BuffsFile="$progdir\Data\Config\buffs.xml"
+$BuffsXml = New-Object System.Xml.XmlDocument
+$BuffsXml.PreserveWhitespace = $true
+$BuffsXml.Load($BuffsFile)
+
+$entityclassesFile="$progdir\Data\Config\entityclasses.xml"
+$entityclassesXml = New-Object System.Xml.XmlDocument
+$entityclassesXml.PreserveWhitespace = $true
+$entityclassesXml.Load($entityclassesFile)
 
 $ItemsFile="$progdir\Data\Config\items.xml"
 $ItemsXml = New-Object System.Xml.XmlDocument
 $ItemsXml.PreserveWhitespace = $true
 $ItemsXml.Load($ItemsFile)
 
-$BuffsFile="$progdir\Data\Config\buffs.xml"
-$BuffsXml = New-Object System.Xml.XmlDocument
-$BuffsXml.PreserveWhitespace = $true
-$BuffsXml.Load($BuffsFile)
+$LootXml = New-Object System.Xml.XmlDocument
+$LootXml.PreserveWhitespace = $true
+$LootXml.Load("$progdir\Data\Config\loot.xml")
 
 function expandedUI {
     # Player craft storage containers
@@ -81,7 +87,7 @@ function expandedUI {
 }
 
 function OPArmor {
-    if ($menu['OPArmor'].Enabled -eq 'true') {
+    if ($menu['OPArmor'].Enabled -in 'true','cheat') {
         $menu['OPArmor'].Enabled = 'false'
         $ItemsXml.Items.SelectNodes("*[@name='plantFiberPants']").SetAttribute('Modded', 'false')
         $ItemsXml.Items.SelectNodes("*[@name='plantFiberPants']/property") | % {
@@ -101,15 +107,21 @@ function OPArmor {
                 "DegradationMax"        {$inp.Value = "72,140" }
             }
         }
+        $ItemsXml.Items.SelectNodes("*[@name='plantFiberHat']/property") | % {
+            $inp=$_
+            switch($_.Name) {
+                "Waterproof"    {$inp.Value = "0.05" }
+            }
+        }
         $ItemsXml.Items.SelectNodes("*[@name='plantFiberHood']/property") | % {
             $inp=$_
             switch($_.Name) {
                 "Waterproof"    {$inp.Value = "0.05" }
             }
         }
-    } else {
-        $menu['OPArmor'].Enabled = 'true'
-        $ItemsXml.Items.SelectNodes("*[@name='plantFiberPants']").SetAttribute('Modded', 'true')
+    } elseif ($menu['cheats'].Enabled -eq 'true') {
+        $menu['OPArmor'].Enabled = 'cheat'
+        $ItemsXml.Items.SelectNodes("*[@name='plantFiberPants']").SetAttribute('Modded', 'cheat')
         $ItemsXml.Items.SelectNodes("*[@name='plantFiberPants']/property") | % {
             $inp=$_
             switch($_.Name) {
@@ -127,10 +139,48 @@ function OPArmor {
                 "DegradationMax"        {$inp.Value = "500,1500" }
             }
         }
+        $ItemsXml.Items.SelectNodes("*[@name='plantFiberHat']/property") | % {
+            $inp=$_
+            switch($_.Name) {
+                "Waterproof"    {$inp.Value = "0.8" }
+            }
+        }
         $ItemsXml.Items.SelectNodes("*[@name='plantFiberHood']/property") | % {
             $inp=$_
             switch($_.Name) {
                 "Waterproof"    {$inp.Value = "0.8" }
+            }
+        }
+    } else {
+        $menu['OPArmor'].Enabled = 'true'
+        $ItemsXml.Items.SelectNodes("*[@name='plantFiberPants']").SetAttribute('Modded', 'true')
+        $ItemsXml.Items.SelectNodes("*[@name='plantFiberPants']/property") | % {
+            $inp=$_
+            switch($_.Name) {
+                "Insulation"    {$inp.Value = "3" } # not too high as easy to overheat
+            }
+        }
+        $ItemsXml.Items.SelectNodes("*[@name='plantFiberPants']/property[@class='Attributes']/property") | % {
+            $inp=$_
+            switch($_.Name) {
+                "ConcussiveProtection"  {$inp.Value = "0.1,0.2" }
+                "PunctureProtection"    {$inp.Value = "0.1,0.2" }
+                "FireProtection"        {$inp.Value = "0.1,0.2" }
+                "RadiationProtection"   {$inp.Value = "0.1,0.2" }
+                "ElectricalProtection"  {$inp.Value = "0.1,0.2" }
+                "DegradationMax"        {$inp.Value = "100,200" }
+            }
+        }
+        $ItemsXml.Items.SelectNodes("*[@name='plantFiberHat']/property") | % {
+            $inp=$_
+            switch($_.Name) {
+                "Waterproof"    {$inp.Value = "0.2" }
+            }
+        }
+        $ItemsXml.Items.SelectNodes("*[@name='plantFiberHood']/property") | % {
+            $inp=$_
+            switch($_.Name) {
+                "Waterproof"    {$inp.Value = "0.2" }
             }
         }
     }
@@ -139,7 +189,7 @@ function OPArmor {
 }
 
 function OPTools {
-    if ($menu['OPTools'].Enabled -eq 'true') {
+    if ($menu['OPTools'].Enabled -in 'true','cheat') {
         $menu['OPTools'].Enabled = 'false'
         $ItemsXml.Items.SelectNodes("*[@name='handPlayer']").SetAttribute('Modded', 'false')
         $ItemsXml.Items.SelectNodes("*[@name='handPlayer']/property[@class='Action0']/property") | % {
@@ -183,9 +233,9 @@ function OPTools {
                 'DegradationRate'   {$inp.Value = '1,1'}
             }
         }
-    } else {
-        $menu['OPTools'].Enabled = 'true'
-        $ItemsXml.Items.SelectNodes("*[@name='handPlayer']").SetAttribute('Modded', 'true')
+    } elseif ($menu['cheats'].Enabled -eq 'true') {
+        $menu['OPTools'].Enabled = 'cheat'
+        $ItemsXml.Items.SelectNodes("*[@name='handPlayer']").SetAttribute('Modded', 'cheat')
         $ItemsXml.Items.SelectNodes("*[@name='handPlayer']/property[@class='Action0']/property") | % {
             $inp=$_
             switch($_.Name) {
@@ -227,13 +277,57 @@ function OPTools {
                 'DegradationRate'   {$inp.Value = '1,1'}
             }
         }
+    } else {
+        $menu['OPTools'].Enabled = 'true'
+        $ItemsXml.Items.SelectNodes("*[@name='handPlayer']").SetAttribute('Modded', 'true')
+        $ItemsXml.Items.SelectNodes("*[@name='handPlayer']/property[@class='Action0']/property") | % {
+            $inp=$_
+            switch($_.Name) {
+                'Range'                 {$inp.Value = '3'}
+                'DamageEntity'          {$inp.Value = '100'}
+                'DamageBlock'           {$inp.Value = '10'}
+                "Stamina_usage"         {$inp.Value = "1" }
+                'DamageBonus.head'      {$inp.Value = '5'}
+            }
+        }
+        $ItemsXml.Items.SelectNodes("*[@name='stoneAxe']/property[@class='Action0']/property") | % {
+            $inp=$_
+            switch($_.Name) {
+                'Range'                 {$inp.Value = '3'}
+                'Block_range'           {$inp.Value = '5'}
+                'Stamina_usage'         {$inp.Value = '2'}
+                'DamageBonus.head'      {$inp.Value = '5'}
+                'DamageBonus.glass'     {$inp.Value = '0.5'}
+                'DamageBonus.wood'      {$inp.Value = '1'}
+                'DamageBonus.earth'     {$inp.Value = '0.4'}
+                'DamageBonus.stone'     {$inp.Value = '0.8'}
+                'DamageBonus.metal'     {$inp.Value = '0.6'}
+                'DamageBonus.organic'   {$inp.Value = '1'}
+                'ToolCategory.harvestingTools'  {$inp.Value = '1.0'}
+            }
+        }
+        $ItemsXml.Items.SelectNodes("*[@name='stoneAxe']/property[@class='Action1']/property") | % {
+            $inp=$_
+            switch($_.Name) {
+                "Repair_amount"     {$inp.Value = "200" }
+            }
+        }
+        $ItemsXml.Items.SelectNodes("*[@name='stoneAxe']/property[@class='Attributes']/property") | % {
+            $inp=$_
+            switch($_.Name) {
+                'EntityDamage'      {$inp.Value = '8,20'}
+                'BlockDamage'       {$inp.Value = '40,60'}
+                'DegradationMax'    {$inp.Value = '200,900'}
+                'DegradationRate'   {$inp.Value = '1,1'}
+            }
+        }
     }
     #$ItemsXml.Save($ItemsFile)
     $ItemsXml.OuterXml | Out-File $ItemsFile -Encoding ASCII
 }
 
 function Buffs {
-    if ($menu['Buffs'].Enabled -eq 'true') {
+    if ($menu['Buffs'].Enabled -in 'true','cheat') {
         $menu['Buffs'].Enabled = 'false'
         $BuffsXml.Buffs.SelectNodes("*[@id='bluePillBuff']").SetAttribute('Modded', 'false')
         $BuffsXml.Buffs.SelectNodes("*[@id='bluePillBuff']/modify") | % {
@@ -274,17 +368,66 @@ function Buffs {
     $BuffsXml.OuterXml | Out-File $BuffsFile -Encoding ASCII
 }
 
+function CorpsesLL {
+    $AfterDeath = @{
+        #These are the ORIGINAL values
+        Backpack                = 1200
+        DroppedLootContainer    = 60
+        zombieTemplateMale      = 30
+        npcSurvivorTemplate     = 45
+        animalTemplateTimid     = 300
+        animalStag              = 300
+        animalTemplateHostile   = 300 
+        npcBanditMelee          = 45
+        npcTraderTemplate       = 45
+        #OLD Pre 16.4 items
+        zombie01                = 30
+        zombieBear              = 30
+        zombiedog               = 30
+        animalBear              = 300
+        hornet                  = 300
+    }
+    if ($menu['CorpsesLL'].Enabled -in 'true','cheat') {
+        $menu['CorpsesLL'].Enabled = 'false'
+        $multiplier=1
+    } elseif ($menu['cheats'].Enabled -eq 'true') {
+        $menu['CorpsesLL'].Enabled = 'cheat'
+        $multiplier=200
+    } else {
+        $menu['CorpsesLL'].Enabled = 'true'
+        $multiplier=20
+    }
+    #$entityclassesXml.entity_classes.SelectNodes("*[@name='Backpack']").SetAttribute('Modded', $menu['CorpsesLL'].Enabled)
+    $entityclassesXml.entity_classes.entity_class | % {
+        if ($AfterDeath[$_.Name]) {
+            #$_.Name
+            $AfterDeathNode=$_.SelectNodes("*[@name='TimeStayAfterDeath']")
+            #$AfterDeathNode
+            if ($AfterDeathNode.value) {
+                #$AfterDeathNode.value = [string]($AfterDeath[$_.Name] * $multiplier)
+                #$_.SelectNodes("*[@name='TimeStayAfterDeath']").value = [string]($AfterDeath[$_.Name] * $multiplier)
+                $AfterDeathNode.SetAttribute('value', [string]($AfterDeath[$_.Name] * $multiplier))
+                #$_.SetAttribute('Modded', $menu['CorpsesLL'].Enabled)
+                $AfterDeathNode.SetAttribute('Modded', $menu['CorpsesLL'].Enabled)
+            }
+        }
+    }
+    $entityclassesXml.OuterXml | Out-File $entityclassesFile -Encoding ASCII
+    #pause
+}
+
 #############################################
 # The Menu
 
 $menu=[ordered]@{
-    #create      = [PSCustomObject]@{F=''                      ;Opt='0' ;A='XML-Archive already exists';I='extract XML-Files only for 1st install or after game-patch'}
-    OPTools     = [PSCustomObject]@{F='items.xml'             ;Opt='1' ;A='';I='OP Starting Tools (hand,stoneAxe'}
-    OPArmor     = [PSCustomObject]@{F='items.xml'             ;Opt='2' ;A='';I="OP Starting Armor (plant)"}
-    Buffs       = [PSCustomObject]@{F='buffs.xml'             ;Opt='3' ;A='';I="Buff Blue Pill: Food,Water,Health"}
-    cheats      = [PSCustomObject]@{F='Actions'               ;Opt='99';A='[Enabled] Cheats';I='Enable Cheat Mode'}
-    #reset       = [PSCustomObject]@{F=''                      ;Opt='X' ;A='';I='reset (if you encounter errors or want to start over)'}
-    exit        = [PSCustomObject]@{F=''                      ;Opt='Z' ;A='';I='end'}
+    #create      = [PSCustomObject]@{F=''                    ;Opt='0' ;A='XML-Archive already exists';I='extract XML-Files only for 1st install or after game-patch'}
+    OPTools     = [PSCustomObject]@{F='items.xml'           ;Opt='1' ;A='';I='OP Starting Tools (hand,stoneAxe)'}
+    OPArmor     = [PSCustomObject]@{F='items.xml'           ;Opt='2' ;A='';I="OP Starting Armor (plant)"}
+    Buffs       = [PSCustomObject]@{F='buffs.xml'           ;Opt='3' ;A='';I="buff bluePillBuff: Food,Water,Health"}
+    CorpsesLL   = [PSCustomObject]@{F='entityclasses.xml'   ;Opt='4' ;A='';I="Corpses Last Longer"}
+    cheats      = [PSCustomObject]@{F='Actions'             ;Opt='99';A='[Enabled] Cheats';I='Enable Cheat Mode'}
+    #reset       = [PSCustomObject]@{F=''                    ;Opt='X' ;A='';I='reset (if you encounter errors or want to start over)'}
+    exit        = [PSCustomObject]@{F=''                    ;Opt='Z' ;A='';I='end'}
 }
 
 #Add Name and Enabled columns to the menu items
@@ -294,6 +437,7 @@ $menu['exit'].Name=''
 $menu['OPTools'].Enabled=$ItemsXml.Items.SelectNodes("*[@name='handPlayer']").Modded
 $menu['OPArmor'].Enabled=$ItemsXml.Items.SelectNodes("*[@name='plantFiberPants']").Modded
 $menu['Buffs'].Enabled=$BuffsXml.Buffs.SelectNodes("*[@id='bluePillBuff']").Modded
+$menu['CorpsesLL'].Enabled=$entityclassesXml.entity_classes.SelectNodes("*[@name='Backpack']").SelectNodes("*[@name='TimeStayAfterDeath']").Modded
 
 #exit
 
