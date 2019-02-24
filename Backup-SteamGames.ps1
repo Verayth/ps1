d5ea967b3c1c1387635bdccfd6205bd527df3360
Verayth <Verayth@users.noreply.github.com>
2019-02-24 17:28:22 -0500

    updated games list

--- Backup-SteamGames.ps1
+++ Backup-SteamGames.ps1
@@ -1,27 +1,44 @@
 
 [CmdletBinding()]param()
 
+$executables=@(
+    'Dungeons3Bin.exe',
+    'CrushCrush.exe',
+    'ConanSandbox.exe',
+    'Fallout4.exe',
+    'Interstellaria.exe',
+    'KingdomCome.exe',
+    'Oblivion.exe',
+    'PillarsOfEternityII.exe',
+    'SkyrimSE.exe',
+    't-engine.exe',
+    't-engine-debug.exe',
+    'tld.exe',
+    'Tyranny.exe'
+)
+
 Get-ItemProperty 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App*\',
                  'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App*\' |
     Where-Object {
         $_.DisplayName -in 'Fallout 4','The Long Dark','Tyranny' -or
-        $_.DisplayName -like '*Skyrim Special Edition' -or
-        $_.DisplayName -like 'Crush Crush' -or
-        $_.DisplayName -like 'Interstellaria' -or
-        $_.DisplayName -like '*eternity*' -or
-        #$_.DisplayName -like '*Starve*' -or
-        $_.DisplayName -like "Tales of Maj'Eyal" -or
         $_.DisplayName -like 'Conan Exiles' -or
-        $_.DisplayName -like '*Oblivion*'
+        $_.DisplayName -like 'Crush Crush' -or
+        $_.DisplayName -like 'Dungeons*' -or
+        $_.DisplayName -like 'Interstellaria' -or
+        $_.DisplayName -like 'Kingdom Come*' -or
+        $_.DisplayName -like '*Oblivion*' -or
+        $_.DisplayName -like 'Pillars of Eternity*' -or
+        $_.DisplayName -like '*Skyrim Special Edition' -or
+        #$_.DisplayName -like '*Starve*' -or
+        $_.DisplayName -like "Tales of Maj'Eyal"
     } | ForEach-Object {
         Write-Host "Checking: $($_.DisplayName)" -ForegroundColor Blue
         #Write-Host $_.InstallLocation
-        $exe=Get-ChildItem $_.InstallLocation | Where-Object Name -in 'Fallout4.exe','SkyrimSE.exe','Oblivion.exe','PillarsOfEternityII.exe',
-            'tld.exe','Tyranny.exe','CrushCrush.exe','ConanSandbox.exe','Interstellaria.exe','t-engine.exe','t-engine-debug.exe'
+        $exe=Get-ChildItem ($_.InstallLocation, "$($_.InstallLocation)\bin\Win64") -ErrorAction SilentlyContinue | Where-Object Name -in $executables
         #,'t-engine.exe'
         if ($exe) {$exe} else {
             Write-Host "'$($_.InstallLocation)'" -ForegroundColor Red
-            Get-ChildItem $_.InstallLocation | Where-Object Name -like '*.exe' |
+            Get-ChildItem ($_.InstallLocation, "$($_.InstallLocation)\bin\Win64") -ErrorAction SilentlyContinue | Where-Object Name -like '*.exe' |
                 Select-Object Name,
                     @{N='FileVersion';E={$_.VersionInfo.FileVersion}},
                     @{N='Created';E={'{0:yyyy-MM-dd}' -f $_.CreationTime}},
